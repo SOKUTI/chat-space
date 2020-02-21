71709a2966cfdd02b20de0c6aeb69f0a80924128
SOKUTI <rsaangou.hirata@gmail.com>
2020-02-21 13:04:51 +0900

    recreate main_chat main and side_bar main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200221005612) do
+ActiveRecord::Schema.define(version: 20200221022337) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20200221005612) do
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "group_id"
-    t.string   "message"
+    t.string   "content"
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
