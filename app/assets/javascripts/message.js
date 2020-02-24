50d5f0e123911aff95ee9e84c89ec701f309dc49
SOKUTI <rsaangou.hirata@gmail.com>
2020-02-24 18:11:51 +0900

    create auto reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,40 +1,77 @@
 $(function() {
   function buildHTML(message) {
-    if ( message.image ) {
+    if (message.content && message.image) {
       var html =
-        `<div class="group">
-          <div class="group__name">
-            ${message.user_name}
-          </div>
-          <div class="group__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="message">
-          <div class="message__content">
-            ${message.content}
-          </div>
-          <img src=${message.image}>
-        </div>`
-      return html;
-    } else {
+        `<div class="group" data-message-id=` + message.id + `>` +
+          `<div class="group__name">` + 
+            message.user_name +
+          `</div>` + 
+          `<div class="group__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message">` +
+          `<div class="message__content">` +
+            message.content +
+          '</div>' +
+          '<img src="' + message.image + '">' +
+        `</div>`
+    } else if (message.content) {
       var html =
-        `<div class="group">
-          <div class="group__name">
-            ${message.user_name}
-          </div>
-          <div class="group__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="message">
-          <div class="message__content">
-            ${message.content}
-          </div>
-        </div>`
-      return html;
+        `<div class="group" data-message-id=` + message.id + `>` +
+          `<div class="group__name">` + 
+            message.user_name +
+          `</div>` + 
+          `<div class="group__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message">` +
+          `<div class="message__content">` +
+            message.content +
+          '</div>' +
+        `</div>`
+    }else {
+      var html =
+        `<div class="group" data-message-id=` + message.id + `>` +
+          `<div class="group__name">` + 
+            message.user_name +
+          `</div>` + 
+          `<div class="group__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message">` +
+          '<img src="' + message.image + '">' +
+        `</div>`
     };
+    return html;
   }
+
+  var reloadMessages = function() {
+    var last_message_id = $(".group:last").data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = "";
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message);
+        });
+        $(".main_chat__main").append(insertHTML);
+        $(".main_chat__main").animate({ scrollTop: $(".main_chat__main")[0].scrollHeight });
+      }
+    })
+    .fail(function() {
+      console.log("error");
+    })
+  }
+
   $("#new_content").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -59,4 +96,7 @@ $(function() {
       $(".main_chat__bottom__send").prop("disabled", false);
     });
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
